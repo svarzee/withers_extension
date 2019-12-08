278259cce26771b57fa6573f634853172e342720
svarzee <svarzee@gmail.com>
2019-12-09 00:46:38 +0100

    Refactor

--- lib/src/withers_generator.dart
+++ lib/src/withers_generator.dart
@@ -9,27 +9,27 @@ class WithersGenerator extends GeneratorForAnnotation<Withers> {
       Element element, ConstantReader annotation, BuildStep buildStep) {
     ClassElement clazz = element;
     final out = StringBuffer();
-    clazz.fields.forEach((field) => out.writeln(generateWither(clazz, field)));
+    clazz.fields.forEach((field) => out.writeln(_generateWither(clazz, field)));
     return out.toString();
   }
 
-  String generateWither(ClassElement clazz, FieldElement field) {
+  String _generateWither(ClassElement clazz, FieldElement field) {
     final witherName =
         'with${field.name[0].toUpperCase()}${field.name.substring(1)}';
-    return '${clazz.name} ${witherName}(${field.type.name} val) => ${clazz.name}${generateCtor(clazz, field)};';
+    return '${clazz.name} ${witherName}(${field.type.name} val) => ${clazz.name}${_generateCtor(clazz, field)};';
   }
 
-  String generateCtor(ClassElement clazz, FieldElement field) {
+  String _generateCtor(ClassElement clazz, FieldElement field) {
     final out = StringBuffer();
     out.write('(');
     out.write(clazz.unnamedConstructor.parameters
-        .map((param) => generateCtorArg(clazz, field, param))
+        .map((param) => _generateCtorArg(clazz, field, param))
         .join(', '));
     out.write(')');
     return out.toString();
   }
 
-  String generateCtorArg(
+  String _generateCtorArg(
       ClassElement clazz, FieldElement field, ParameterElement param) {
     return field.name == param.name
         ? 'val'
